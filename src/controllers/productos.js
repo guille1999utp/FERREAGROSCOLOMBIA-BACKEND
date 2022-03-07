fb2aad16502376cc143f35b049a0b086fe890a23
guille1999utp <guillermo.penaranda@utp.edu.co>
2022-03-07 10:30:50 -0500

    borrador completo mercadolibr

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,6 +1,4 @@
 const Producto = require('../models/producto');
-const {mercadopago} = require('../utils/mercadoPago');
-const Usuario = require('../models/usuario');
 const Categoria = require('../models/categoria');
 
 const categorias = async (req,res) => {  
@@ -230,112 +228,7 @@ try {
             }
   
 
-          const PagarServicios = async(req,res) =>{
-            const miId = req.uid;
-            const usuario = await Usuario.findById(miId);
-            console.log(usuario)
-            let preference = {
-              items: [
-                {
-                  title: 'LB-SHOP',
-                  unit_price: usuario.dinerosolicitudes,
-                  quantity: 1,
-                  description: 'Recibo Solicitudes'
-                }
-              ],
-              back_urls: {
-                "success": `http://localhost:3000/feedback/${miId}`,
-                "failure": `http://localhost:3000/feedback/${miId}`,
-                "pending": `http://localhost:3000/feedback/${miId}`
-              },
-              auto_return: "approved"
-            };
-            mercadopago.preferences.create(preference)
-            .then(function (response) {
-              // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
-              console.log(response)
-              res.json({
-                global: response.body.id
-                });
-            })
-            .catch(function (error) {
-              console.log(error);
-            });              
-
-          }
-
-          const PagarProducto = async (req,res) => {
-            const categoriabuscar = req.params.id;
-            const datos = req.body.items;
-            const producto = await Producto.findById( categoriabuscar );
-            let preference = {
-              transaction_amount: parseInt(producto.detalles[0].Precio*1.15),
-                net_amount: parseInt((producto.detalles[0].Precio*1.15)*0.968-800),
-                taxes:[{
-                        value: parseInt(producto.detalles[0].Precio*1.15)-parseInt((producto.detalles[0].Precio*1.15)*0.968-800),
-                        type: "IVA"
-                }],
-              binary_mode: true,
-              payer:{
-              name: datos.nombre,
-              surname:datos.apellidos,
-              email: datos.email,
-              phone:{
-              number: parseInt(datos.telefono),
-              area_code: "57",
-              },
-              address:{
-                zip_code: datos.postal,
-                street_name: datos.Barrio,
-                street_number: parseInt(datos.street_number)
-              },
-        
-              },
-              shipments:{
-                receiver_address:{
-                  zip_code:datos.postal,
-                  street_name: datos.Barrio,
-                  street_number: parseInt(datos.street_number),
-                  floor: datos.floor,
-                  apartment: datos.apartment,
-                  city_name: datos.ciudad,
-                  state_name: datos.estado,
-                  country_name: 'colombia'
-                }
-              },
-              additional_info: datos.anadirinfo,
-              items: [
-                  { 
-                      picture_url: datos.picture_url,
-                      title: producto.titulo,
-                      unit_price: parseInt(producto.detalles[0].Precio*1.15),
-                      quantity: 1,
-                      description: producto.textdescripsion[0]
-                  }
-              ],
-              back_urls: {
-                  "success": `http://localhost:3000/feedback/${categoriabuscar}`,
-                  "failure": `http://localhost:3000/feedback/${categoriabuscar}`,
-                  "pending": `http://localhost:3000/feedback/${categoriabuscar}`
-              },
-              auto_return: "approved",
-          };
-    
-              mercadopago.preferences.create(preference)
-              .then(function (response) {
-                // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
-                console.log(response)
-                res.json({
-                  global: response.body.id,
-                  postman: response
-                });
-              })
-              .catch(function (error) {
-                console.log(error);
-              });              
-
-            }
-    
+       
             const FeedBack = async (req,res) => {
               res.json({
                 ok:true,
@@ -352,9 +245,7 @@ module.exports ={
     pedirproducto,
     informacionAdicional,
     informacionmostrarcategoria,
-    PagarProducto,
     FeedBack,
-    PagarServicios,
     categorias,
     categoriasindividual,
     CategoriasTodas
