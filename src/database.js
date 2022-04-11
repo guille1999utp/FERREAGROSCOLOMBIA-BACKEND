5d24950b8b02e22c3a0a4444668bf8ca1481e78c
guille1999utp <guillermo.penaranda@utp.edu.co>
2022-04-11 09:09:49 -0500

    conectada

--- src/database.js
+++ src/database.js
@@ -7,5 +7,5 @@ mongoose.connect(process.env.MONGODB_CNN, {
 const connection = mongoose.connection;
 
 connection.once('open', ()=>{
-    console.log('DB esta conectada');
+    console.log('DB esta conectada correctamente');
 });
